68b4266997de91c86de7fc634b7fbaa2f8f6fc7e
Alexey Kuchmenko <alex@alex.al>
2025-06-08 15:39:35 +0700

    fix update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
-docker compose down
-git pull
-docker compose build
-docker compose up -d
+# есть новый "docker compose" ?
+DC="docker compose"
+`$DC version > /dev/null 2>&1` || DC="docker-compose"
+
+$DC down
+$DC pull
+$DC build
+$DC up -d
