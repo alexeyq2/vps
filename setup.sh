6a8d71ccdb05b194519252be34d9ad29df8982d1
Alexey Kuchmenko <alex@alex.al>
2025-06-08 15:37:31 +0700

    better comment

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # sharry process runs as ordinary user with id=10001 in container
 chown 10001 srv/sharry/h2_data srv/sharry/files
 
-# limit size of journal logs (docker logs may grow) /var/log/journal
+# limit size of logs (docker logs may grow) /var/log/journal
 grep "SystemMaxUse=100M" /etc/systemd/journald.conf >/dev/null || echo "SystemMaxUse=100M" >> /etc/systemd/journald.conf
 systemctl restart systemd-journald
 
