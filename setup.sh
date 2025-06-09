0273d0feb17bb728a1a2348fe9c72227fd523b5a
Alexey Kuchmenko <alex@alex.al>
2025-06-10 03:24:18 +0700

    remove sharry from setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
-# sharry process runs as ordinary user with id=10001 in container
-chown 10001 srv/sharry/h2_data srv/sharry/files
-
 # limit size of logs (docker logs may grow) /var/log/journal
 grep "SystemMaxUse=100M" /etc/systemd/journald.conf >/dev/null || echo "SystemMaxUse=100M" >> /etc/systemd/journald.conf
 systemctl restart systemd-journald
