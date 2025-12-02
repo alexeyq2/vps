3d2d4b089c1bb362a93146f51eeaccc9d9098084
Alexey Kuchmenko <alex@alex.al>
2025-12-02 21:41:07 +0700

    certbot main loop  improved error handling;  small log size to chatty filebrowser

--- certbot/main.py
+++ certbot/main.py
@@ -7,14 +7,23 @@ os.system('envsubst < cli.ini.template > /etc/letsencrypt/cli.ini')
 
 print('certbot renew loop start', datetime.now())
 
- # создать/обновить сертификат
-if os.system('certbot certonly'): 
-  print('certbot certonly failed')
-  exit(2)
+time.sleep(random.randint(1, 7))  # не сразу после запуска контейнера стучаться на letsencrypt
+
+seconds_in_hour = 60 * 60
+
+# создать/обновить сертификат
+# если не удалось - ждать несколько часов и пробовать снова, не выходя из скрипта, 
+# чтобы контейнер не перезапускался часто и не долбил letsencrypt, иначе забанят.
+# если такое случится, они пришлют письмо на email, указанный при создании сертификата с инструкцией как разблокировать
+while os.system('certbot certonly'):
+  some_hours = (1 + random.randint(0, 4)) * seconds_in_hour
+  print('ERROR certbot certonly. Wait', some_hours // 60, 'minutes before retry')
+  time.sleep(some_hours)
+  print(datetime.now())
+  
 
 # раз в пару дней проверять обновление
 while True:
-  seconds_in_hour = 60 * 60
   day_or_two = (24 + random.randint(0, 24)) * seconds_in_hour
   time.sleep(day_or_two)
   print(datetime.now())
