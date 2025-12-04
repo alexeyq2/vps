ca668c16ea0a12306598634478fe298cd4f70369
Alexey Kuchmenko <alex@alex.al>
2025-12-04 08:50:02 +0700

    fix git url in get.sh

--- get.sh
+++ get.sh
@@ -31,7 +31,7 @@ else
 fi
 
 sudo apt install -y git-core mc curl wget htop
-git clone git@github.com:alexeyq2/vps.git
+git clone https://github.com/alexeyq2/vps.git
 cd vps
 git checkout $RELEASE
 
