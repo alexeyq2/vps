b0d83f26b8c41a4286d8d8b0a3105bb00748e4f7
Alexey Kuchmenko <alex@alex.al>
2026-01-27 09:45:11 +0700

    add FILEZ_URL

--- filebrowser-delete-data.sh
+++ filebrowser-delete-data.sh
@@ -1,3 +1,3 @@
 docker compose stop filebrowser
 sudo rm -rf srv/filebrowser/*
-docker compose start filebrowser && docker compose logs filebrowser | grep password
+docker compose start filebrowser && sleep 3 && docker compose logs filebrowser | grep password
